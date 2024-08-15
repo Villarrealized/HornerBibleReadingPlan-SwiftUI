263a2bda17217a427c6fafb84585031dcef6ccd0
Nicholas Villarreal <nvillarreal@protonmail.com>
2024-08-14 18:16:12 -0600

    Initial Commit

---
+++ HornerBibleReadingPlan/HornerBibleReadingPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HornerBibleReadingPlanApp.swift
+//  HornerBibleReadingPlan
+//
+//  Created by Nicholas Villarreal on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct HornerBibleReadingPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
